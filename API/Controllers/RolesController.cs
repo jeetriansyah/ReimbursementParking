dabd364c3331cf5117009eb0bb50c742dcea61ab
jeetriansyah <jeetriansyah96@gmail.com>
2019-12-26 14:12:49 +0700

    View Role done

---
+++ API/Controllers/RolesController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Services;
+using API.Services.Interface;
+using Data.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RolesController : ControllerBase
+    {
+        IRoleService roleService = new RoleService();
+
+        // GET: api/Roles
+        [HttpGet]
+
+        public IEnumerable<Role> Get()
+        {
+            return roleService.Get();
+        }
+
+        // GET: api/Roles/5
+        //[HttpGet("{id}", Name = "Get")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        // POST: api/Roles
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Roles/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- Client/Controllers/DepartmentsController.cs
+++ Client/Controllers/DepartmentsController.cs
@@ -1,18 +1,111 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+using Client.Base;
+using Data.Model;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Web;
+using Data.ViewModel;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
 
 namespace Client.Controllers
 {
     public class DepartmentsController : Controller
     {
+        Port getPort = new Base.Port();
+        readonly HttpClient client = new HttpClient();
         // GET: Departments
         public ActionResult Index()
         {
-            return View();
+            var Id = HttpContext.Session.GetString("Id");
+            if (Id != null)
+            {
+                return View(List());
+            }
+            return RedirectToAction("Login", "Users");
+        }
+
+        public JsonResult List()
+        {
+            IEnumerable<Department> departments = null;
+            var client = new HttpClient
+            {
+                BaseAddress = new Uri(getPort.client)
+            };
+            var responseTask = client.GetAsync("Departments");
+            responseTask.Wait();
+            var result = responseTask.Result;
+            if (result.IsSuccessStatusCode)
+            {
+                var readTask = result.Content.ReadAsAsync<IList<Department>>();
+                readTask.Wait();
+                departments = readTask.Result;
+            }
+            else
+            {
+                departments = Enumerable.Empty<Department>();
+                ModelState.AddModelError(string.Empty, "server error, try after some time");
+            }
+            return Json(departments);
+        }
+
+        public JsonResult InsertOrUpdate(DepartmentVM departmentVM)
+        {
+            var client = new HttpClient
+            {
+                BaseAddress = new Uri(getPort.client)
+            };
+            var myContent = JsonConvert.SerializeObject(departmentVM);
+            var buffer = System.Text.Encoding.UTF8.GetBytes(myContent);
+            var byteContent = new ByteArrayContent(buffer);
+            byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            if (departmentVM.Id == 0)
+            {
+                var result = client.PostAsync("Departments", byteContent).Result;
+                return Json(result);
+            }
+            else
+            {
+                var result = client.PutAsync("Departments/" + departmentVM.Id, byteContent).Result;
+                return Json(result);
+            }
+        }
+
+        public JsonResult GetById(int id)
+        {
+            Department department = null;
+            var client = new HttpClient
+            {
+                BaseAddress = new Uri(getPort.client)
+            };
+            var responseTask = client.GetAsync("Departments/" + id);
+            responseTask.Wait();
+            var result = responseTask.Result;
+            if (result.IsSuccessStatusCode)
+            {
+                var readTask = result.Content.ReadAsAsync<Department>();
+                readTask.Wait();
+                department = readTask.Result;
+            }
+            else
+            {
+                // try to find something
+            }
+            return Json(department);
+        }
+
+        public JsonResult Delete(int id)
+        {
+            var client = new HttpClient
+            {
+                BaseAddress = new Uri(getPort.client)
+            };
+            var result = client.DeleteAsync("Departments/" + id).Result;
+            return Json(result);
         }
 
         // GET: Departments/Details/5
@@ -68,10 +161,10 @@ namespace Client.Controllers
         }
 
         // GET: Departments/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
+        //public ActionResult Delete(int id)
+        //{
+        //    return View();
+        //}
 
         // POST: Departments/Delete/5
         [HttpPost]
